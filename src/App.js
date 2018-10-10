0a05a796e70a52a74459285f319563a26efec5a0
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-10-10 17:04:20 -0400

    create a new app called todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+
+  constructor(props){
+  	super(props);
+  	this.state = {
+      newTodo:'',
+      todos: [
+        {id: 1, name: 'eat foods and exercise'},
+        {id: 2, name: 'buy some clothes'},
+        {id: 3, name: 'write some codes'},
+        {id: 4, name: 'get ready for some interviews'},
+        {id: 5, name: 'be positive in life'}
+    ]
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+handleChange(event){
+  this.setState({
+    newTodo: event.target.value
+  });
+  //console.log(event.target.name, event.target.value)
+}
+
+addTodo(){
+  const newTodo = {
+    name: this.state.newTodo,
+    id: this.state.todos[this.state.todos.length -1].id + 1
+  };
+
+const todos = this.state.todos;
+  todos.push(newTodo);
+
+  this.setState({
+    todos: todos,
+    newTodo: ''
+  });
+
+}
+
+
   render() {
+    console.log(this.state.newTodo)
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <div className="container">
+
+          <input
+            type="text"
+            name="todo"
+            className="m-4 form-control"
+            placeholder="Add a new to TODO"
+            onChange={this.handleChange}
+            value={this.state.newTodo}
+            />
+          <button
+            onClick={this.addTodo}
+            className="btn-info mb-3 form-control"
+            >
+            ADD todo
+          </button>
+
+
+
+          <ul className="list-group">
+              {this.state.todos.map((item)=>{
+                return <li key={item.id} className="list-group-item">{item.name}</li>
+              })
+              }
+
+          </ul>
+
+        </div>
       </div>
     );
   }
